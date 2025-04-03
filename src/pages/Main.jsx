0b56eb7e4c1b1c5f69f3af82bb0e5efdc95c3762
hypark023 <hypark023@gmail.com>
2025-04-03 15:43:42 +0900

    commit before branch switch

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,13 +6,17 @@ const Main = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // sessionStorage에 저장된 경로 가져오기
+    // 깃헙 배포환경에서 라우터 문제 해결 
+    // 404페이지에서 저장한 sessionStorage 저장 경로 가져오기
+    
     const redirectPath = sessionStorage.getItem("redirectPath");
+
     if (redirectPath) {
       sessionStorage.removeItem("redirectPath"); // 한 번만 실행되도록 삭제
       navigate(redirectPath, { replace: true }); // 원래 경로로 이동
     }
   }, [navigate]);
+  
   return (
     <div className="container text-center bg-light ">
       <h1>Main</h1>
