c33fa76fbee4609a234b18761733725ab68008c8
hypark023 <hypark023@gmail.com>
2025-04-03 13:54:43 +0900

    add 404

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,9 +1,10 @@
-import { Suspense, lazy } from "react";
+import { Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../pages/Main";
+import Sample from "../pages/sample/Sample";
 
 const Loading = <div>Loading..</div>;
-const Sample = lazy(() => import("../pages/sample/sample")); // lazy는 해당 모듈이 필요할때만 동적으로 로드하는 기능, Main 컴포넌트를 초기 번들에 포함하지 않고, 사용자가 해당 페이지에 접근할 때 비동기적으로 로드됨, 초기 로딩 속도를 줄이고 성능 최적화 효과가 있음
+//const Sample = lazy(() => import("../pages/sample/sample")); // lazy는 해당 모듈이 필요할때만 동적으로 로드하는 기능, Main 컴포넌트를 초기 번들에 포함하지 않고, 사용자가 해당 페이지에 접근할 때 비동기적으로 로드됨, 초기 로딩 속도를 줄이고 성능 최적화 효과가 있음
 const root = createBrowserRouter(
   [
     {
