aaa944c910b56cd83f788e7a2bf606a60566a439
hypark023 <hypark023@gmail.com>
2025-04-11 16:47:06 +0900

    path chagne

---
+++ src/pages/Waiting.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Waiting = () => {
+  return (
+    <div className="p-4 text-center ">
+      <div className="mt-lg-5 mb-lg-5">
+        <h1>준비중입니다. 🛠🔧</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Waiting;
--- src/router/root.jsx
+++ src/router/root.jsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../pages/Main";
 import Sample from "../pages/sample/Sample";
+import Waiting from "../pages/waiting";
 
 const Loading = <div>Loading..</div>;
 //const Sample = lazy(() => import("../pages/sample/sample")); // lazy는 해당 모듈이 필요할때만 동적으로 로드하는 기능, Main 컴포넌트를 초기 번들에 포함하지 않고, 사용자가 해당 페이지에 접근할 때 비동기적으로 로드됨, 초기 로딩 속도를 줄이고 성능 최적화 효과가 있음
@@ -17,13 +18,21 @@ const root = createBrowserRouter(
       //<Suspense>는 **로딩 중일 때 표시할 UI (fallback 속성)**를 지정하는 역할을 함.
     },
     {
-      path: "/sample",
+      path: "/onlyosse",
       element: (
         <Suspense fallback={Loading}>
           <Sample></Sample>
         </Suspense>
       ),
     },
+    {
+      path: "/sample",
+      element: (
+        <Suspense fallback={Loading}>
+          <Waiting></Waiting>
+        </Suspense>
+      ),
+    }
   ],
   {
     basename: import.meta.env.BASE_URL, // 깃허브 페이지스를 위한 설정
